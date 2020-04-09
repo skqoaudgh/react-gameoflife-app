f0a02716abcca050afe2ffdffdad48cf4ea9c83b
myungho <skqoaudgh2@hanmail.net>
2020-04-09 17:23:51 +0900

    Grid 및 Box 기본 구조 작성

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./App.css";
+
+import Grid from './Grid';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
+    this.speed = 100;
+    this.rows = 30;
+    this.cols = 50;
+
     this.state = {
       generation: 0,
-    }
+      gridFull: Array(this.rows)
+        .fill()
+        .map((_) => Array(this.cols).fill(false)),
+    };
   }
 
   render() {
     return (
       <div>
         <h1>The Game of Life</h1>
-        <Grid />
+        <Grid
+          rows={this.rows}
+          cols={this.cols}
+          gridFull={this.state.gridFull}
+          selectBox={this.selectBox}
+        />
         <h2>Generations: {this.state.generation}</h2>
       </div>
     );
